b49ab351b4235e2e794fa5f63baec865f02fb0b0
clementbouly <clementbouly@hotmail.fr>
2022-12-17 17:18:33 +0100

    refacto(): improve code to respect DRY

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,54 +1,88 @@
-let currentCalculationData = ""
 let currentResult = 0
+const logEntries = []
 
-addBtn.addEventListener("click", () => {
-	if (currentCalculationData === "") {
-		currentCalculationData = userInput.value
-	} else {
-		currentCalculationData += " + " + userInput.value
-	}
-	currentResult += parseInt(userInput.value)
-	outputResult(currentResult, currentCalculationData)
-})
+function getFloatUserInput() {
+	return parseFloat(userInput.value)
+}
 
-subtractBtn.addEventListener("click", () => {
-	currentCalculationData += " - " + userInput.value
-
-	currentResult -= parseInt(userInput.value)
-	outputResult(currentResult, currentCalculationData)
-})
-
-multiplyBtn.addEventListener("click", () => {
-	if (currentCalculationData === "") {
-		currentCalculationData = "0 * " + userInput.value
-	} else {
-		currentCalculationData += " * " + userInput.value
-	}
-
-	currentResult *= parseInt(userInput.value)
-	outputResult(currentResult, currentCalculationData)
-})
-
-divideBtn.addEventListener("click", () => {
-	if (userInput.value === "0") {
+function isUserInputValid(operator) {
+	if (operator === "/" && getFloatUserInput() === 0) {
 		alert("You can't divide by 0!")
+		return false
+	}
+	if (isNaN(getFloatUserInput())) {
+		alert("Please enter a valid number!")
+		return false
+	} else {
+		return true
+	}
+}
+
+function createAndWriteOutput(operator) {
+	if (!isUserInputValid(operator)) {
 		return
 	}
 
-	if (currentCalculationData === "") {
-		currentCalculationData = "0 / " + userInput.value
-	} else {
-		currentCalculationData += " / " + userInput.value
+	const input = getFloatUserInput()
+	const initialResult = currentResult
+
+	switch (operator) {
+		case "+":
+			currentResult += input
+			break
+		case "-":
+			currentResult -= input
+			break
+		case "*":
+			currentResult *= input
+			break
+		case "/":
+			currentResult /= input
+			break
+		default:
+			alert("Invalid operator!")
+			break
 	}
 
-	currentResult /= parseInt(userInput.value)
+	let currentCalculationData = `${initialResult} ${operator} ${input}`
+
+	const logEntry = {
+		operation: operator,
+		prevResult: initialResult,
+		number: input,
+		result: currentResult,
+		calculation: currentCalculationData,
+	}
+	logEntries.push(logEntry)
+
 	outputResult(currentResult, currentCalculationData)
-})
+}
 
-resetBtn.addEventListener("click", () => {
-    currentCalculationData = ""
-    currentResult = 0
-    outputResult(currentResult, currentCalculationData)
-})
+function add() {
+	createAndWriteOutput("+")
+}
 
+function substract() {
+	createAndWriteOutput("-")
+}
 
+function multiply() {
+	createAndWriteOutput("*")
+}
+
+function divide() {
+	createAndWriteOutput("/")
+}
+
+function reset() {
+	currentCalculationData = ""
+	currentResult = 0
+	outputResult(currentResult, currentCalculationData)
+	console.log(logEntries)
+}
+
+addBtn.addEventListener("click", add)
+subtractBtn.addEventListener("click", substract)
+multiplyBtn.addEventListener("click", multiply)
+divideBtn.addEventListener("click", divide)
+resetBtn.addEventListener("click", reset)
