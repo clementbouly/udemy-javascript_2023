a14c074e4b4812406d7a2897f756858abf63ee8a
clementbouly <clementbouly@hotmail.fr>
2023-01-11 09:51:22 +0100

    feat(dom-06): end implementation

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,27 +1,45 @@
-const addModal = document.querySelector("#add-modal")
+const addMovieModal = document.getElementById("add-modal")
 const confirmDeleteModal = document.querySelector("#delete-modal")
 
 const openAddMovieModal = document.querySelector("header button")
-const cancelModalButton = addModal.querySelector(".btn--passive")
-const addMovieButton = addModal.querySelector(".btn--success")
+const cancelModalButton = addMovieModal.querySelector(".btn--passive")
+const addMovieButton = addMovieModal.querySelector(".btn--success")
 const main = document.querySelector("main")
 const movieList = document.querySelector("#movie-list")
+const backdrop = document.querySelector("#backdrop")
+const entryTextSection = document.querySelector("#entry-text")
 
 let movieSelectedId = null
 
-// MODAL
+const updateUI = () => {
+	if (movieList.children.length === 0) {
+		entryTextSection.style.display = "block"
+	} else {
+		entryTextSection.style.display = "none"
+	}
+}
 
-const toggleModal = (modal) => {
-	modal.classList.toggle("visible")
+// MODAL
+const toggleBackdrop = () => {
+	backdrop.classList.toggle("visible")
 	main.classList.toggle("blur")
 }
 
-openAddMovieModal.addEventListener("click", () => toggleModal(addModal))
+const toggleModal = (modal) => {
+	modal.classList.toggle("visible")
+	toggleBackdrop()
+}
 
-cancelModalButton.addEventListener("click", () => toggleModal(addModal))
+backdrop.addEventListener("click", () => {
+	if (addMovieModal.classList.contains("visible")) toggleModal(addMovieModal)
+	if (confirmDeleteModal.classList.contains("visible")) toggleModal(confirmDeleteModal)
+})
+
+openAddMovieModal.addEventListener("click", () => toggleModal(addMovieModal))
+
+cancelModalButton.addEventListener("click", () => toggleModal(addMovieModal))
 
 // CONFIRM DELETE MODAL
-
 const cancelDeleteButton = confirmDeleteModal.querySelector(".btn--passive")
 const confirmDeleteButton = confirmDeleteModal.querySelector(".btn--danger")
 
@@ -32,7 +50,6 @@ confirmDeleteButton.addEventListener("click", () => {
 })
 
 // DELETE MOVIE FUNCTION
-
 const deleteMovie = (id) => {
 	const movie = document.getElementById(id)
 	if (!movie) return
@@ -40,13 +57,12 @@ const deleteMovie = (id) => {
 }
 
 // CREATE MOVIE ELEMENT
-
 const createMovieElement = (title, imageUrl, rating) => {
 	const id = Math.random().toString(36).substring(2, 9)
 
 	imageUrl = imageUrl || "https://picsum.photos/160/"
-	rating = rating || Math.floor(Math.random() * 5)
-	title = title || "No Title - " + id
+	rating = rating && rating >= 0 && rating <= 5 ? rating : Math.floor(Math.random() * 5)
+	title = title.trim() || "No Title - " + id
 
 	const movieElement = document.createElement("li")
 	movieElement.className = "movie-element"
@@ -71,22 +87,22 @@ const createMovieElement = (title, imageUrl, rating) => {
 		movieSelectedId = id
 	})
 
-	movieList.appendChild(movieElement)
-	toggleModal(addModal)
+	movieList.append(movieElement)
+	toggleModal(addMovieModal)
 }
 
 // FORM SUBMISSION
-
 const movieTitleInput = document.querySelector("#title")
 const imageUrlInput = document.querySelector("#image-url")
 const ratingInput = document.querySelector("#rating")
 
 addMovieButton.addEventListener("click", () => {
 	createMovieElement(movieTitleInput.value, imageUrlInput.value, ratingInput.value)
+	updateUI()
 })
 
 // TEST DATA
 // createMovieElement("Harry Potter", "https://i.ebayimg.com/images/g/nHQAAOSwQ8hhoMr~/s-l1600.jpg", 5)
 // createMovieElement("Harry Potter 2", "https://picsum.photos/160", 4)
 // createMovieElement("Harry Potter 3", "https://picsum.photos/160", 3)
-// toggleModal(addModal)
+// toggleModal(addMovieModal)
